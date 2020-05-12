a67d7ce1b3a69d2c828fc93bb0e1f5c9a90b2a92
v0k00i1 <victoriyak@aspectiva.com>
2020-05-12 16:15:07 +0300

    oron commit 2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json())
 
 const MongoClient = require('mongodb').MongoClient;
-
+//dsgjds;gd
 MongoClient.connect(config.mongodbUrl, { promiseLibrary: Promise })
     .catch(err => console.error(err.stack))
     .then(db => {
